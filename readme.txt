0ad0130e5c538502dcacc0510f065ea491e083fc
robttay <robttay@yahoo.com>
2017-07-01 21:59:55 -0500

    Finalized routing.

--- readme.txt
+++ readme.txt
@@ -1,6 +1,11 @@
 
+Using Express, Mustache, and express-session,
+create an app with a login page. When a user goes to /,
+and they are not logged in, redirect them to /login.
+Upon entering a valid username and password,
+they should be authenticated and sent back to /.
+The root page should show that they are logged in
+and what username they are logged in as.
+The valid usernames and passwords should be kept as
+a data structure in your application.
 
-
-get "/" successfully reroutes to /login if not logged in
-
-NEXT STEP: uncomment code to authenticate post from "/login"
--- server.js
+++ server.js
@@ -7,6 +7,10 @@ const sessionConfig = require("./sessionConfig");
 const port = process.env.PORT || 8000;
 var users = [
   {
+    username: "pete",
+    password: "pete"
+  },
+  {
     username: "rockybalboa",
     password: "adrian"
   },
@@ -27,7 +31,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(session(sessionConfig));
 
 function checkAuth(req, res, next) {
-  if (!request.session.user) {
+  if (!req.session.user) {
     return res.redirect("/login");
   } else {
     next();
@@ -35,67 +39,38 @@ function checkAuth(req, res, next) {
 }
 
 // routes
-app.get("/", function(req, res) {
-  if (!req.body || !req.body.username || !req.body.password) {
-    console.log("missing data");
-    return res.redirect("/login");
-  }
-  res.render("index");
+app.get("/", checkAuth, function(req, res) {
+  res.render("index", { user: req.session.user });
 });
-
-// app.get("/signup", function(req, res) {
-//   res.render("signup");
-// });
-// ///////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 app.get("/login", function(req, res) {
   res.render("login");
 });
 
-// app.get("/profile", function(req, res) {
-//   res.render("profile", { user: req.session.user });
-// });
-
-// app.post("/login", checkAuth, function(req, res) {
-//   if (!req.body || !req.body.username || !req.body.password) {
-//     console.log("missing data");
-//     return res.redirect("/login");
-//   }
-//   var requestingUser = req.body;
-//   var userRecord;
-
-//   users.forEach(function(item) {
-//     console.log(item);
-//     if (item.username === requestingUser.username) {
-//       userRecord = item;
-//     }
-//   });
-
-//   if (!userRecord) {
-//     console.log("No record");
-//     return res.redirect("/login");
-//   }
-
-//   if (requestingUser.password === userRecord.password) {
-//     req.session.user = userRecord;
-//     return res.redirect("/profile");
-//   } else {
-//     console.log("Wrong password");
-//     return res.redirect("/login");
-//   });
-
-// app.post("/users", function(req, res) {
-//   if (!req.body || !req.body.username || !req.body.password) {
-//     return res.redirect("/");
-//   }
-//   var newUser = {
-//     username: req.body.username,
-//     password: req.body.password
-//   };
-//   users.push(newUser);
-//   console.log("users: ", users);
-//   return res.redirect("login");
-// });
+app.post("/login", function(req, res) {
+  if (!req.body || !req.body.username || !req.body.password) {
+    console.log("missing data");
+    return res.redirect("/login");
+  }
+  var requestingUser = req.body;
+  var userRecord;
+  users.forEach(function(item) {
+    if (item.username === requestingUser.username) {
+      userRecord = item;
+    }
+  });
+  if (!userRecord) {
+    console.log("No record");
+    return res.redirect("/login");
+  }
+  if (requestingUser.password === userRecord.password) {
+    req.session.user = userRecord;
+    return res.redirect("/");
+  } else {
+    console.log("Wrong password");
+    return res.redirect("/login");
+  }
+});
 
 app.listen(port, function() {
   console.log("Server is running on port: ", port);
